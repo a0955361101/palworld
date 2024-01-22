1791bae50fb240b7964257df919df878c3e1bf46
Bryce <a0955361101@gmail.com>
2024-01-22 17:33:33 +0800

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
-
+  import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
+  <NavBar :paths="[
+        { id: 0, pageName: '帕魯詞綴', url: '/' },
+        // { id: 1, pageName: '寶可夢', url: '/pokemon' },
+    ]">
+    </NavBar>
   <router-view></router-view>
 </template>
 
