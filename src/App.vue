b86cf6185646e869c1182848880836170a34455e
Bryce <a0955361101@gmail.com>
2024-01-22 17:54:01 +0800

    update css

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,5 @@
 </template>
 
 <style>
-  #app{
-    max-width: 1200px;
-    margin: 0 auto;
-  }
+  
 </style>
--- src/pages/affixList.vue
+++ src/pages/affixList.vue
@@ -18,29 +18,39 @@ import { ref } from 'vue';
 </script>
 
 <template>
-    <div class="affixList">
-        <div class="data_list" :class="changeColor(item.type)" v-for="item in affixData" :key="item.skillName">
-            <div class="skill">技能: {{ item.skillName }}</div>
-            <div class="ability">能力: {{ item.ability }}</div>
+    <div class="affixList_box">
+        <div class="affixList">
+            <div class="data_list" :class="changeColor(item.type)" v-for="item in affixData" :key="item.skillName">
+                <div class="skill">技能: {{ item.skillName }}</div>
+                <div class="ability">能力: {{ item.ability }}</div>
+            </div>
         </div>
     </div>
+    
     
 </template>
 
 <style lang="scss" scoped>
-.affixList{
-    margin: 3vh 0;
+.affixList_box{
+    max-width: 1200px;
+    margin: 0 auto;
+    .affixList{
     width: 100%;
     display: flex;
     flex-wrap: wrap;
     .data_list{
         display: flex;
-        width: 33%;
-        padding: 1vh 0;
+        width: 32%;
+        padding: 1.5vh 0 1.5vh 0.5vw;
         border: 1px solid #333;
+        @media (max-width: 375px) {
+            width: 100%;
+            padding: 3vh 0 3vh 1vw;
+        }
         .skill{
             width: 35%;
         }
+        
     }
     
     .red{
@@ -50,5 +60,7 @@ import { ref } from 'vue';
         background: #efd684;
     }
 }
+}
+
 
 </style>
